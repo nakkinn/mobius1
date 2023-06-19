3b912e7545433b5742edd6e4e0a04973530b6d50
nakkinn <81859514+nakkinn@users.noreply.github.com>
2023-06-19 15:54:36 +0900

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,105 +1,107 @@
-function handleTouchMove(event) {
-    event.preventDefault();
-}
-//スクロール禁止
-document.addEventListener('touchmove', handleTouchMove, { passive: false });
+//3Dモデルの作成、更新をするクラス
+class Iuvmodel{
 
-const canvasElement = document.querySelector('#myCanvas')
-const renderer = new THREE.WebGLRenderer({
-canvas: document.querySelector('#myCanvas')
-});
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
+    //オブジェクト生成時に呼び出される
+    constructor(uvvts, index, fx, fy, fz){
+ 
+        this.uvvts = uvvts;
+        this.index = index;
+        this.fx = fx;
+        this.fy = fy;
+        this.fz = fz;
 
-let scene = new THREE.Scene();
+        let tmp;
 
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight);
-camera.position.set(0, 0, +1000);
+        //ジオメトリの生成
+        this.geometry = new THREE.BufferGeometry();
 
-const controls = new THREE.OrbitControls(camera, canvasElement);
+        //ポリゴンの頂点番号を設定
+        if(index[0].length==4){ //頂点リストが4角ポリゴンで表している場合、3角ポリゴンに変換
+            tmp = [];
+            for(let i=0; i<index.length; i++){
+                tmp.push(index[i][0],index[i][1],index[i][3]);
+                tmp.push(index[i][2],index[i][3],index[i][1]);
+            }
+            this.index = tmp.concat();
+        }else   this.index = index.flat(1);
 
-//
-//GUI
-//
-const datGUIObj = {
-    para:0
-};
+        this.geometry.setIndex(this.index);  
 
-const gui = new dat.GUI({ autoPlace: true, width: 600});
-gui.domElement.id = 'gui';
-console.log(gui);
+        //ポリゴンの頂点座標を設定
+        tmp = [];
+        for(let i=0; i<uvvts.length; i++)   tmp.push(fx(this.uvvts[i][0],this.uvvts[i][1]), fy(this.uvvts[i][0],this.uvvts[i][1]), fz(this.uvvts[i][0],this.uvvts[i][1]));
+        this.geometry.setAttribute('position', new THREE.BufferAttribute(new Float32Array(tmp), 3));
 
-gui.add(datGUIObj,'para',0, Math.PI);
+        //頂点法線の計算
+        this.geometry.computeVertexNormals();   
 
-//
-//メインジオメトリ
-//
-let geometry = new THREE.BufferGeometry();
+        //マテリアルの設定
+        this.material = new THREE.MeshNormalMaterial({side:THREE.DoubleSide});
+        this.material.flatShading = true;   //フラットシェード
 
-let detail = 40;
+        //メッシュ（ジオメトリ＋マテリアル）の生成
+        this.mesh = new THREE.Mesh(this.geometry, this.material);
 
-let v0 = [];
-let v = []
-for(let i=0; i<detail; i++){
-    v0[i] = [];
-    v[i] = [];
-    for(let j=0; j<detail; j++){
-        v[i][j] = [];
-        v0[i][j] = [];
-        let x = i/(detail-1)*Math.PI - Math.PI/2;
-        let y = j/(detail-1)*2*Math.PI;
-
-        v0[i][j][0]=Math.cos(x)*Math.cos(y);
-        v0[i][j][1]=Math.cos(x)*Math.sin(y);
-        v0[i][j][2]=Math.sin(x)*Math.cos(y/2);
-        v0[i][j][3]=Math.sin(x)*Math.sin(y/2);
+        //シーンにメッシュを追加
+        scene.add(this.mesh);   
+ 
     }
+
+    
+ 
+    //座標の更新
+    Iupdate(){
+
+        //ポリゴンの頂点座標の再設定
+        let tmp = [];
+        for(let i=0; i<this.uvvts.length; i++)   tmp.push(this.fx(this.uvvts[i][0],this.uvvts[i][1]), this.fy(this.uvvts[i][0],this.uvvts[i][1]), this.fz(this.uvvts[i][0],this.uvvts[i][1]));
+        this.geometry.setAttribute('position', new THREE.BufferAttribute(new Float32Array(tmp), 3));
+
+        //頂点法線の再計算
+        this.geometry.computeVertexNormals();
+        
+    }
+
+    //最長辺がthresholdより大きいポリゴンを非表示にする
+    Iremovehugemesh(threshold){
+    
+        let vlist = this.geometry.attributes.position.array;
+        let indexcopy = this.index.concat();
+        let x1, y1, z1, x2, y2, z2, x3, y3, z3;
+        let maxdis;
+
+        function Idist(a,b,c,d,e,f){
+            return Math.sqrt((a-d)*(a-d)+(b-e)*(b-e)+(c-f)*(c-f));
+        }
+
+        for(let i=0; i<indexcopy.length; i+=3){
+            x1 = vlist[indexcopy[i]*3];
+            y1 = vlist[indexcopy[i]*3+1];
+            z1 = vlist[indexcopy[i]*3+2];
+            x2 = vlist[indexcopy[i+1]*3];
+            y2 = vlist[indexcopy[i+1]*3+1];
+            z2 = vlist[indexcopy[i+1]*3+2];
+            x3 = vlist[indexcopy[i+2]*3];
+            y3 = vlist[indexcopy[i+2]*3+1];
+            z3 = vlist[indexcopy[i+2]*3+2];
+
+            maxdis = Math.max(Idist(x1,y1,z1,x2,y2,z2),Idist(x3,y3,z3,x2,y2,z2),Idist(x1,y1,z1,x3,y3,z3));
+
+            if(maxdis > threshold){
+                indexcopy.splice(i,3);
+                i -= 3;
+            }
+
+        }
+
+        this.geometry.setIndex(indexcopy);
+   
+    }
+
 }
 
-let angle=Math.PI/4;
-for(let i=0;i<detail;i++)   for(let j=0;j<detail;j++){
-    let temx=v0[i][j][0];
-    let temy=v0[i][j][3];
-    v0[i][j][0]=temx*Math.cos(angle)-temy*Math.sin(angle);
-    v0[i][j][3]=temx*Math.sin(angle)+temy*Math.cos(angle);
-}
 
-let tem = [];
-for(let i=0; i<detail; i++) for(let j=0; j<detail; j++) tem.push(v0[i][j][0]/(1-v0[i][j][3])*150, v0[i][j][1]/(1-v0[i][j][3])*150, v0[i][j][2]/(1-v0[i][j][3])*150);
-let vertices = new Float32Array(tem);
-
-
-let indices = [];
-for(let i=0; i<detail-1 ;i++) for(let j=0; j<detail-1; j++){
-    indices.push(i*detail+j, i*detail+(j+1)%detail, (i+1)%detail*detail+j);
-    indices.push((i+1)%detail*detail+(j+1)%detail, (i+1)%detail*detail+j, i*detail+(j+1)%detail);
-}
-
-
-geometry.setIndex( indices );
-
-geometry.setAttribute( 'position', new THREE.BufferAttribute( vertices, 3 ) );
-
-geometry.computeVertexNormals();
-
-
-for(let i=0; i<detail; i++){
-    geometry.attributes.normal.array[((detail-1-i)*detail+detail-1)*3] = -geometry.attributes.normal.array[i*detail*3];
-    geometry.attributes.normal.array[((detail-1-i)*detail+detail-1)*3+1] = -geometry.attributes.normal.array[i*detail*3+1];
-    geometry.attributes.normal.array[((detail-1-i)*detail+detail-1)*3+2] = -geometry.attributes.normal.array[i*detail*3+2];
-}
-
-
-
-const material = new THREE.MeshNormalMaterial( { color: 0xff0000, side: THREE.DoubleSide } );
-//material.flatShading = true;
-let mesh = new THREE.Mesh( geometry, material );
-scene.add(mesh);
-
-//
 //境界のチューブ
-//
-
 class mycurve extends THREE.Curve {
 
     constructor(rota) {
@@ -108,6 +110,8 @@ class mycurve extends THREE.Curve {
 	}
 
 	getPoint( t, optionalTarget = new THREE.Vector3() ) {
+
+        this.rota = mygui.t;
 
         let s = t*Math.PI*4;
 
@@ -122,103 +126,100 @@ class mycurve extends THREE.Curve {
         tx = tx1*Math.cos(this.rota)-tw1*Math.sin(this.rota);
         tw = tx1*Math.sin(this.rota)+tw1*Math.cos(this.rota);
 
-		return optionalTarget.set( tx/(1-tw)*150, ty/(1-tw)*150, tz/(1-tw)*150 );
+		return optionalTarget.set( tx/(1-tw)*100, ty/(1-tw)*100, tz/(1-tw)*100 );
 	}
 }
 
-let path = new mycurve(Math.PI/4);
+
+
+//レンダラー
+const canvasElement = document.querySelector('#myCanvas');
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#myCanvas')
+});
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(960, 540); //画面の横、縦サイズ
+
+
+//クリッピング
+let cpsize = 300;   //クリッピングする立方体の大きさ（辺長/2）
+
+let cplane1 = new THREE.Plane(new THREE.Vector3(1,0,0),cpsize);
+let cplane2 = new THREE.Plane(new THREE.Vector3(-1,0,0),cpsize);
+let cplane3 = new THREE.Plane(new THREE.Vector3(0,1,0),cpsize);
+let cplane4 = new THREE.Plane(new THREE.Vector3(0,-1,0),cpsize);
+let cplane5 = new THREE.Plane(new THREE.Vector3(0,0,-1),cpsize);
+let cplane6 = new THREE.Plane(new THREE.Vector3(0,0,1),cpsize);
+
+renderer.clippingPlanes.push(cplane1,cplane2,cplane3,cplane4,cplane5,cplane6);
+
+
+//シーン
+const scene = new THREE.Scene();
+
+
+//カメラ
+const camera = new THREE.PerspectiveCamera(45, 960/540); //射影カメラ　引数：(視野角、アスペクト比)
+camera.position.set(0, 0, 1000);    //カメラ位置
+const controls = new THREE.OrbitControls(camera, canvasElement);    //マウスによる視点操作
+
+
+//スライダー
+const mygui = {
+    t:Math.PI/4,    //変数名：初期値
+};
+const gui = new dat.GUI({height:30});
+gui.add(mygui,'t', 0.000001, Math.PI-0.00001); //スライダーを追加　引数：（mygui, '変数名', 最小値, 最大値）
+
+
+//(u,v)から(x,y,z)への３つの写像
+//注：パラメータ t は mygui.t とかく
+//   三角関数 sin は Math.sin() と表す
+//   時間（経過フレーム数）：　renderer.info.rendere.frame
+function Ifunc1x(u,v){
+    return 100 * (Math.cos(mygui.t)*Math.cos(u)*Math.cos(v) - Math.sin(mygui.t)*Math.sin(u)*Math.sin(v/2)) / (1 - Math.sin(mygui.t)*Math.cos(u)*Math.cos(v) - Math.cos(mygui.t)*Math.sin(u)*Math.sin(v/2));
+}
+function Ifunc1y(u,v){
+    return 100 * Math.cos(u)*Math.sin(v) / (1 - Math.sin(mygui.t)*Math.cos(u)*Math.cos(v) - Math.cos(mygui.t)*Math.sin(u)*Math.sin(v/2));
+}
+function Ifunc1z(u,v){
+    return 100 * Math.sin(u)*Math.cos(v/2) / (1 - Math.sin(mygui.t)*Math.cos(u)*Math.cos(v) - Math.cos(mygui.t)*Math.sin(u)*Math.sin(v/2));
+}
+
+
+//モデルの生成・追加
+//( R^2上のuv座標, ポリゴンの頂点番号, 関数(u,v)→x, (u,v)→y, (u,v)→z )
+let mobius1 = new Iuvmodel(uvvts1, index1, Ifunc1x, Ifunc1y, Ifunc1z);
+
+
+//チューブ
+let path = new mycurve(mygui.t);
 let geometry1 = new THREE.TubeGeometry( path, 60, 5, 8, false );
 const material1 = new THREE.MeshBasicMaterial( { color: 0xff8800 , side:THREE.DoubleSide} );
-
 let mesh1 = new THREE.Mesh( geometry1, material1 );
 scene.add( mesh1 );
 
-//
-//クリッピング
-//
 
-let plane1 = new THREE.Plane(new THREE.Vector3(1,0,0),500);
-let plane2 = new THREE.Plane(new THREE.Vector3(-1,0,0),500);
-let plane3 = new THREE.Plane(new THREE.Vector3(0,1,0),500);
-let plane4 = new THREE.Plane(new THREE.Vector3(0,-1,0),500);
-let plane5 = new THREE.Plane(new THREE.Vector3(0,0,-1),500);
-let plane6 = new THREE.Plane(new THREE.Vector3(0,0,1),500);
+//描画処理（毎フレーム行われる）
+Ianimation();
 
-renderer.clippingPlanes.push(plane1,plane2,plane3,plane4,plane5,plane6);
+function Ianimation(){
 
-tick();
+    mobius1.Iupdate();  //スライダーや時間によって変形するモデルの更新 
+    mobius1.Iremovehugemesh(1000);  //巨大なメッシュを削除
 
-function tick() {
-    
-    angle = datGUIObj.para;
-
-    for(let i=0;i<detail;i++)   for(let j=0;j<detail;j++){
-        v[i][j][1] = v0[i][j][1];
-        v[i][j][2] = v0[i][j][2];
-        v[i][j][0]=v0[i][j][0]*Math.cos(angle)-v0[i][j][3]*Math.sin(angle);
-        v[i][j][3]=v0[i][j][0]*Math.sin(angle)+v0[i][j][3]*Math.cos(angle);
-        
-    }
-
-    let tem = [];
-    for(let i=0; i<detail; i++) for(let j=0; j<detail; j++) tem.push(v[i][j][0]/(1-v[i][j][3])*150, v[i][j][1]/(1-v[i][j][3])*150, v[i][j][2]/(1-v[i][j][3])*150);
-    let vertices = new Float32Array(tem);
-
-    indices = [];
-    for(let i=0; i<detail-1 ;i++) for(let j=0; j<detail-1; j++){
-
-        let x1,x2,x3,y1,y2,y3,z1,z2,z3;
-        x1 = vertices[(i*detail+j)*3];
-        y1 = vertices[(i*detail+j)*3+1];
-        z1 = vertices[(i*detail+j)*3+2];
-        x2 = vertices[(i*detail+(j+1)%detail)*3];
-        y2 = vertices[(i*detail+(j+1)%detail)*3+1];
-        z2 = vertices[(i*detail+(j+1)%detail)*3+2];
-        x3 = vertices[((i+1)%detail*detail+j)*3];
-        y3 = vertices[((i+1)%detail*detail+j)*3+1];
-        z3 = vertices[((i+1)%detail*detail+j)*3+2];
-
-        let d = (x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)+(z1-z2)*(z1-z2)+(x3-x2)*(x3-x2)+(y3-y2)*(y3-y2)+(z3-z2)*(z3-z2);
-
-        if(d<1000000){
-            indices.push(i*detail+j, i*detail+(j+1)%detail, (i+1)%detail*detail+j);
-            indices.push((i+1)%detail*detail+(j+1)%detail, (i+1)%detail*detail+j, i*detail+(j+1)%detail);
-        }
-    }
-
-
-    geometry.setIndex( indices );
-    geometry.setAttribute( 'position', new THREE.BufferAttribute( vertices, 3 ) );
-
-    geometry.computeVertexNormals();
-    for(let i=0; i<detail; i++){
-        geometry.attributes.normal.array[((detail-1-i)*detail+detail-1)*3] = -geometry.attributes.normal.array[i*detail*3];
-        geometry.attributes.normal.array[((detail-1-i)*detail+detail-1)*3+1] = -geometry.attributes.normal.array[i*detail*3+1];
-        geometry.attributes.normal.array[((detail-1-i)*detail+detail-1)*3+2] = -geometry.attributes.normal.array[i*detail*3+2];
-    }
-    
-    
-    //
     //チューブの更新
-    //
-
     scene.remove( mesh1 );
-    path = new mycurve(Math.PI/4 + datGUIObj.para);
+    path = new mycurve(mygui.t);
+    path.getPoint();
     geometry1 = new THREE.TubeGeometry( path, 40, 5, 8, false );
     mesh1 = new THREE.Mesh( geometry1, material1 );
     scene.add( mesh1 );
-    
 
-    //
-    //レンダリング
-    //
-    renderer.render(scene, camera); 
-    requestAnimationFrame(tick);
+    renderer.render(scene, camera); //レンダリング
+    requestAnimationFrame(Ianimation);  //Ianimationを繰り返す
 
 }
 
-/*
-renderer.info.rendere.frame:framecount 
-*/
 
 
